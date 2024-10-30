965c3ea6ad0170473f3ec1e4a6a387653ee4a042
vishalrathore8oct <vishal.rathore8oct@gmail.com>
2024-10-30 16:43:47 +0530

    set up backend project part-2

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("main server file running");
+import 'dotenv/config'
+import express from "express";
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get("/", (req, res) => {
+    res.status(200).send(`<h1>Hello This is our Home Page of MyStreamTube App</h1>`)
+})
+
+app.listen(port, () => {
+    console.log(`Your Server is Runnig on Port http://localhost:${port}`);
+})
